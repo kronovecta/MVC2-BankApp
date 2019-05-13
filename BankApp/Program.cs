a3bef4b0a2f78ca08f35e647fe3fd40a342ebb5b
Robert Nordgren <robert.b.nordgren@gmail.com>
2019-05-13 15:22:17 +0200

    Added console app and basic API fetch

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BankApp.Data;
+using BankApp.Domain.Entities;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace BankApp
 {
@@ -6,7 +10,44 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool alive = true;
+            var utils = new Utilities();
+
+            Console.Write("Enter a client name: ");
+            var response = Console.ReadLine();
+
+            Console.Write("How many results: ");
+            int.TryParse(Console.ReadLine(), out int limit);
+
+            var pagenr = 1;
+
+            while (alive)
+            {
+                var query = utils.GetCustomersNamed(response, limit);
+
+                foreach (var customer in query.customers)
+                {
+                    Console.WriteLine($"[ID: {customer.CustomerId}] Name: {customer.Givenname} {customer.Surname}");
+                    foreach (var disposition in customer.Dispositions)
+                    {
+                        Console.WriteLine($"\t{disposition.Account.Created.ToString()}");
+                    }
+                }
+
+                Console.WriteLine("\nPress enter for more results, type 'exit' to quit");
+                var res = Console.ReadLine();
+
+                switch (res)
+                {
+                    default:
+                        //offset += limit;
+                        pagenr++;
+                        continue;
+                    case "exit":
+                        alive = false;
+                        break;
+                }
+            }   
         }
     }
 }
